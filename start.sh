f2d4a513deba12bf7e5589e76cd9d86bc5e193ec
haroldhang <haroldadjahi@gmail.com>
2023-07-08 19:30:58 +0100

    new config app

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!bin/bash
 
-/etc/init.d/nginx configtest
-/etc/init.d/nginx start
+#/etc/init.d/nginx configtest
+#/etc/init.d/nginx start
 #mysqld
-cat /etc/php/7.4/fpm/pool.d/www.conf
-ps aux
+#cat /etc/php/7.4/fpm/pool.d/www.conf
+#ps aux
 #ls /etc/init.d
+
+/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 /var/www/build.sh
