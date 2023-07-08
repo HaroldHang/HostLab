b346b460ea660d3d68df2e93e35ed212cf425ef5
haroldhang <haroldadjahi@gmail.com>
2023-07-08 11:45:36 +0100

    still coming

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 /etc/init.d/nginx configtest
 /etc/init.d/nginx start
 #mysqld
+cat /etc/php/7.4/fpm/pool.d/www.conf
+ps aux
 ls /etc/init.d
 /var/www/build.sh
-ps aux
-cat /etc/php/7.4/fpm/pool.d/www.conf
